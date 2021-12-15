b27ded72aca0b0db6d7e733b4746ed2ccff35c82
tommyskott <tommy@borgenfalk.se>
2021-12-15 20:26:10 +0100

    Added login event handler

--- components/AuthContext.js
+++ components/AuthContext.js
@@ -12,6 +12,12 @@ export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
+    netlifyIdentity.on("login", (user) => {
+      setUser(user);
+      netlifyIdentity.close();
+      console.log("login event");
+    });
+
     // init netlify identity
     netlifyIdentity.init();
   }, []);

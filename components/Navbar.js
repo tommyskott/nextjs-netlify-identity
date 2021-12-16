cc8e5c7b8ff2b241300b67a1e5da3d51def19c5a
tommyskott <tommy@borgenfalk.se>
2021-12-16 17:51:15 +0100

    navbar: render ul if authReady

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,35 +3,37 @@ import { useContext } from "react";
 import AuthContext from "./AuthContext";
 
 export default function Navbar() {
-  const { user, login, logout } = useContext(AuthContext);
+  const { user, login, logout, authReady } = useContext(AuthContext);
   console.log(user);
 
   return (
     <>
       <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/vip">
-              <a>VIP</a>
-            </Link>
-          </li>
-          {!user && (
+        {authReady && (
+          <ul>
             <li>
-              <button onClick={login}>Login</button>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
             </li>
-          )}
-          {user && <li>{user.email}</li>}
-          {user && (
             <li>
-              <button onClick={logout}>Logout</button>
+              <Link href="/vip">
+                <a>VIP</a>
+              </Link>
             </li>
-          )}
-        </ul>
+            {!user && (
+              <li>
+                <button onClick={login}>Login</button>
+              </li>
+            )}
+            {user && <li>{user.email}</li>}
+            {user && (
+              <li>
+                <button onClick={logout}>Logout</button>
+              </li>
+            )}
+          </ul>
+        )}
       </nav>
     </>
   );

683e698528889a9308435c46030299243d02c55b
tommyskott <tommy@borgenfalk.se>
2021-12-17 15:52:12 +0100

    updated data

--- functions/premiumContent.js
+++ functions/premiumContent.js
@@ -1,11 +1,28 @@
 exports.handler = async (event, context) => {
   console.log("getting premium content...");
 
-  const data = {
-    cool: "yeah",
-    awesome: "sweet",
-    hey: 38,
-  };
+  const data = [
+    {
+      title: "Dribbble",
+      body: "Donec id elit non mi porta gravida at eget.",
+      link: "https://dribbble.com/",
+    },
+    {
+      title: "Github",
+      body: "Duis mollis, est non commodo luctus, nisi erat.",
+      link: "https://github.com/",
+    },
+    {
+      title: "Behance",
+      body: "Nullam id dolor id nibh ultricies ut id elit.",
+      link: "https://www.behance.net/",
+    },
+    {
+      title: "Unsplash",
+      body: "Sed posuere consectetur est at lobortis.",
+      link: "https://unsplash.com/",
+    },
+  ];
 
   if (context.clientContext.user) {
     return {
